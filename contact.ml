5ad83f2c4a9cd5ee9e86a583186cd3b71047bda7
Quentin Baudet <quentin.baudet@epitech.com>
2017-04-11 17:09:05 +0200

    Fichier + débt de contact

---
+++ contact.ml
@@ -0,0 +1,5 @@
+type field = All | Id| FirstName | LastName | Age | Email | Phone
+
+type contact = (string * string * int * string * string)
+
+let contact = (FirstName , LastName , Age , Email , Phone)
---
+++ contact.mli
@@ -0,0 +1,5 @@
+module type CONTACT =
+  sig
+    type contact;;
+  end
+module Contact : CONTACT
